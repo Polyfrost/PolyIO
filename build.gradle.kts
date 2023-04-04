e565d1cb16aa89194cd07e696d8ba7ff720435f5
xtrm <oss@xtrm.me>
2023-04-05 00:04:08 +0200

    🐛 fix(build): tidy dependencies, remove unused test

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,17 +15,19 @@ val repoId = "polyfrost/PolyIO"
 
 repositories {
     maven("https://repo.polyfrost.cc/releases")
+    mavenCentral()
 }
 
 dependencies {
 //    implementation("fr.stardustenterprises:plat4k:1.6.3")
-    implementation("org.jetbrains:annotations:24.0.0")
+    implementation("org.jetbrains:annotations:24.0.1")
+    @Suppress("GradlePackageUpdate")
     implementation("org.apache.logging.log4j:log4j-api:2.0-beta9")
-	runtimeOnly("org.apache.logging.log4j:log4j-core:2.0-beta9")
+    @Suppress("GradlePackageUpdate")
+    testRuntimeOnly(runtimeOnly("org.apache.logging.log4j:log4j-core:2.0-beta9")!!)
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testRuntimeOnly("org.apache.logging.log4j:log4j-core:2.17.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
 java {
--- src/test/java/cc/polyfrost/polyio/tests/BestTest.java
+++ src/test/java/cc/polyfrost/polyio/tests/BestTest.java
@@ -35,9 +35,4 @@ public class BestTest {
         Path path = dl.get();
         System.out.println("Downloaded at " + path);
     }
-
-    @SneakyThrows
-    @Test
-    public void test2() {
-    }
 }
